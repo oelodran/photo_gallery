4a36b4baae858a43041167fe18e7b3d1ec2bebe1
Leonardo Ramljak <leonardo.ramljak@tel.net.ba>
2019-04-03 11:33:15 +0200

    Designing public/index page with bootstrap

--- public/staff/index.php
+++ public/staff/index.php
@@ -9,14 +9,24 @@ require_once ('../../private/initialize.php');
 
 $photos = Photo::find_all();
 ?>
+<?php include (SHARED_PATH . '/staff_header.php'); ?>
 
-<table>
+<table class="table table-bordered table-responsive-md table-striped text-center">
+    <tr>
+        <th class="text-center">Name</th>
+        <th class="text-center">Photos</th>
+        <th class="text-center">Created at</th>
+        <th>&nbsp;</th>
+        <th>&nbsp;</th>
+        <th>&nbsp;</th>
+    </tr>
     <?php foreach ($photos as $photo) { ?>
     <tr>
-        <td><?php echo h($photo->name); ?></td>
-        <td><?php echo '<img src="data:image; base64,' . h(base64_encode($photo->image)) . '"'; ?></td>
-        <td><?php echo h($photo->created_at); ?></td>
+        <td class="text-center"><?php echo h($photo->name); ?></td>
+        <td class="d-lg-table-cell img-thumbnail"><?php echo '<img src="data:image; base64,' . h(base64_encode($photo->image)) . '"'; ?></td>
+        <td class="text-center"><?php echo h($photo->created_at); ?></td>
     </tr>
     <?php } ?>
 </table>
 
+<?php include (SHARED_PATH . '/staff_footer.php'); ?>
